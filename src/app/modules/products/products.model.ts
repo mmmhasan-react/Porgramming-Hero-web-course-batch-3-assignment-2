1fca59db9f7c6ead6fa77025693408340ecdb608
mmhasan-react <2md.mahmudul.hasan@gmail.com>
2024-06-10 07:33:25 +0600

    create product schema

---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, connect } from "mongoose";
+import { Product, ProductName } from "./products.interface";
+
+const productNameSchema = new Schema<ProductName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+const productSchema = new Schema<Product>({
+  productName: productNameSchema,
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: { type: [String], required: true },
+  variants: [
+    {
+      type: { type: String, required: true },
+      value: { type: String, required: true },
+    },
+  ],
+  inventory: {
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
+  },
+});
